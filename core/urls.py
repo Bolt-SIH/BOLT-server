d007aab533e7d41afebcee1f46414e0bae6dec16
Theskyspace <akashrockzz411@gmail.com>
2022-08-15 03:30:02 -0400

    Intial setup commit

---
+++ core/urls.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from django.urls import path
+
+from rest_framework import permissions
+from drf_yasg2.views import get_schema_view
+from drf_yasg2 import openapi
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Bolt APIs",
+      default_version='v1',
+      contact=openapi.Contact(email="akash.joshi@sayf.in"),
+   ),
+    public=False,
+    # permission_classes=(permissions.IsAuthenticated, permissions.IsAdminUser),
+    # authentication_classes=(authentication)
+)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    # path('', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+]
