db1566f9728cbf79d8bbeb5eaa2659eeec2143e2
Theskyspace <akashrockzz411@gmail.com>
2022-08-25 18:09:56 -0400

    Change in the schema of the entire app

--- content/articles.py
+++ content/articles.py
@@ -12,6 +12,8 @@ from rest_framework import views, response, status, permissions, authentication
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
 
 
+from content import models
+
 @swagger_auto_schema(
     method="GET",
     responses=responses.GET_RESPONSES,
@@ -22,49 +24,23 @@ from rest_framework.decorators import api_view, authentication_classes, permissi
 @permission_classes([])
 def fetch_articles(request):
     # Dummy data
+    Article_Model = models.Article.objects.all()
+    article = []
+    for i in Article_Model:
+        article.append({
+        "source": i.source,
+        "author": i.source,
+        "title": i.title,
+        "description": i.description,
+        "url": i.sourceURL,
+        "urlToImage": request.build_absolute_uri(i.image.url),
+        "publishedAt": None,
+        "content": i.description
+        },) 
+
     data = {
     "status": "ok",
-    "totalResults": 2,
-    "articles": [
-    {
-    "source": "The Star Online",
-    "author": "Glenn CHAPMAN",
-    "title": "Elon Musk lawyers seize on Twitter whistleblower revelations",
-    "description": "Elon Musk’s lawyers jumped Aug 24 on the revelations of a Twitter whistleblower to try to force the platform to surrender vast amounts of information for their fight to cancel the billionaire’s buyout bid. Read full story",
-    "url": "https://www.thestar.com.my/tech/tech-news/2022/08/25/elon-musk-lawyers-seize-on-twitter-whistleblower-revelations",
-    "urlToImage": "https://apicms.thestar.com.my/uploads/images/2022/08/25/1710897.jpg",
-    "publishedAt": "2022-08-25T04:35:00Z",
-    "content": "SAN FRANCISCO: Elon Musks lawyers jumped Aug 24 on the revelations of a Twitter whistleblower to try to force the platform to surrender vast amounts of information for their fight to cancel the billi… [+2991 chars]"
-    },
-    {
-    "source": "The Star Online",
-    "author": "Glenn CHAPMAN",
-    "title": "Elon Musk lawyers seize on Twitter whistleblower revelations",
-    "description": "Elon Musk’s lawyers jumped Aug 24 on the revelations of a Twitter whistleblower to try to force the platform to surrender vast amounts of information for their fight to cancel the billionaire’s buyout bid. Read full story",
-    "url": "https://www.thestar.com.my/tech/tech-news/2022/08/25/elon-musk-lawyers-seize-on-twitter-whistleblower-revelations",
-    "urlToImage": "https://apicms.thestar.com.my/uploads/images/2022/08/25/1710897.jpg",
-    "publishedAt": "2022-08-25T04:35:00Z",
-    "content": "SAN FRANCISCO: Elon Musks lawyers jumped Aug 24 on the revelations of a Twitter whistleblower to try to force the platform to surrender vast amounts of information for their fight to cancel the billi… [+2991 chars]"
-    },
-    {
-    "source": "The Star Online",
-    "author": "Glenn CHAPMAN",
-    "title": "Elon Musk lawyers seize on Twitter whistleblower revelations",
-    "description": "Elon Musk’s lawyers jumped Aug 24 on the revelations of a Twitter whistleblower to try to force the platform to surrender vast amounts of information for their fight to cancel the billionaire’s buyout bid. Read full story",
-    "url": "https://www.thestar.com.my/tech/tech-news/2022/08/25/elon-musk-lawyers-seize-on-twitter-whistleblower-revelations",
-    "urlToImage": "https://apicms.thestar.com.my/uploads/images/2022/08/25/1710897.jpg",
-    "publishedAt": "2022-08-25T04:35:00Z",
-    "content": "SAN FRANCISCO: Elon Musks lawyers jumped Aug 24 on the revelations of a Twitter whistleblower to try to force the platform to surrender vast amounts of information for their fight to cancel the billi… [+2991 chars]"
-    },
-    {
-    "source" : "The Star Online",
-    "author": "Glenn CHAPMAN",
-    "title": "Elon Musk lawyers seize on Twitter whistleblower revelations",
-    "description": "Elon Musk’s lawyers jumped Aug 24 on the revelations of a Twitter whistleblower to try to force the platform to surrender vast amounts of information for their fight to cancel the billionaire’s buyout bid. Read full story",
-    "url": "https://www.thestar.com.my/tech/tech-news/2022/08/25/elon-musk-lawyers-seize-on-twitter-whistleblower-revelations",
-    "urlToImage": "https://apicms.thestar.com.my/uploads/images/2022/08/25/1710897.jpg",
-    "publishedAt": "2022-08-25T04:35:00Z",
-    "content": "SAN FRANCISCO: Elon Musks lawyers jumped Aug 24 on the revelations of a Twitter whistleblower to try to force the platform to surrender vast amounts of information for their fight to cancel the billi… [+2991 chars]"
-    }
-    ]}
+        "totalResults": len(Article_Model),
+        "articles": article}
+        
     return response.Response(data , status.HTTP_200_OK)
